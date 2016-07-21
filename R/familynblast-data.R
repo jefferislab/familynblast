c9b69e90a097069b513ec35c1c587e8a1ebb448c
Gregory Jefferis <jefferis@gmail.com>
2016-07-21 12:30:40 +0100

    Document data in package

---
+++ R/familynblast-data.R
@@ -0,0 +1,35 @@
+#' All flycircuit neurons with the correct labels we are working on
+#' @name fc_neuron_typec
+#' @docType data
+NULL
+
+#' All 137 families of flycircuit neurons
+#' @details We still have to get rid of the ones that contains one neuron
+#' @name correct_families
+#' @docType data
+NULL
+
+#' FIXME
+#' @name probability_correct_families
+#' @docType data
+NULL
+
+#' The numbers of neurons crossing each supervoxel, matrix of 7065
+#' @name number_neurons_fromfam_insv
+#' @docType data
+NULL
+
+#' The numbes of neurons crossing each supervoxel, matrix of 7065
+#' @name number_neurons_fromfam_insv
+#' @docType data
+NULL
+
+#' The probabilities of all the supervoxels in each family, matrix of nrow = 7065
+#' @name probability_sv_knowing_family
+#' @docType data
+NULL
+
+#' FIXME
+#' @name svoxels.fcwb.table
+#' @docType data
+NULL

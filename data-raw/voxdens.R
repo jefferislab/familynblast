91bfebcfa941e22d3ae4c114364c509b8dc4838e
Gregory Jefferis <jefferis@gmail.com>
2016-07-21 17:18:06 +0100

    data: fix loading of voxel_dens_allneurons

--- data-raw/voxdens.R
+++ data-raw/voxdens.R
@@ -1,5 +1,6 @@
 library(Matrix)
+load("/Volumes/JData5/JPeople/Melina/Branson/data/voxel_dens_allneurons")
 voxel_dens_allneurons=Matrix(voxel_dens_allneurons, sparse=T)
 
 library(devtools)
-use_data(voxel_dens_allneurons)
+use_data(voxel_dens_allneurons, overwrite = T)

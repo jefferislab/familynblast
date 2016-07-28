0fac14d633137b16bd06dde35ce862508014982b
Gregory Jefferis <jefferis@gmail.com>
2016-07-28 11:50:08 +0100

    add test against precalculated families
    * this new test works
    * but the tests for new families still fail

--- tests/testthat/test-families.R
+++ tests/testthat/test-families.R
@@ -3,8 +3,13 @@ context("families")
 test_that("calculation of family probability matrices works", {
   kcs20.setoffamilies=by(names(kcs20), kcs20[,'type'], as.character)
 
+  # Melina had the input neurons as the names of the vectors, which is not so
+  # generally useful
+  correct_families2=sapply(correct_families, names, simplify = F)
+  expect_equal(create_probab_sv_knowing_fam(correct_families2[2:4]),
+               probability_sv_knowing_family[,2:4], tol=1e-3)
 
-  expect_equal_to_reference(kcs20.prob <- create_probab_sv_knowing_fam(kcs20.setoffamilies),
-                            "testdata/kcs20.prob.rds")
+  kcs20.prob <- create_probab_sv_knowing_fam(kcs20.setoffamilies)
+  expect_equal_to_reference(kcs20.prob, "testdata/kcs20.prob.rds")
   expect_equal(create_probab_sv_knowing_fam(kcs20.setoffamilies, db=kcs20), kcs20.prob)
 })
